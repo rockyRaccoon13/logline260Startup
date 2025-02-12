b545d74e533f6b26910c49d208334d639934f248
Kurt Rowley <bensonro@byu.edu>
2025-02-11 17:45:54 -0700

    create app component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,46 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-dark text-light">
+      <header className="container-fluid">
+        <nav className="navbar fixed-top navbar-dark">
+          <div className="navbar-brand">Logline</div>
+          <menu className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/index.html">
+                Login/Register
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/reviews.html">
+                Reviews
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/viewProfile.html">
+                User's profile
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/editProfile.html">
+                Edit user's profile
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/publishReview.html">
+                Publish new review
+              </a>
+            </li>
+          </menu>
+        </nav>
+        <hr />
+      </header>
+
+      <footer className="fixed-bottom">
+        <span className="text-reset">Benson Rowley</span>
+        <a href="https://github.com/rockyRaccoon13/logline260Startup">GitHub</a>
+      </footer>
+    </div>
+  );
 }
